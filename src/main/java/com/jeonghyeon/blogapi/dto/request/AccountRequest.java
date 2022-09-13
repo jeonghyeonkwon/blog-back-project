dfc65b5b9250f2b1ee4a9c125b475e5c2cf0aba8
jeonghyeonkwon <givejeong2468@gmail.com>
2022-09-13 18:04:37 +0900

    dto swagger 추가

--- src/main/java/com/jeonghyeon/blogapi/dto/request/AccountRequest.java
+++ src/main/java/com/jeonghyeon/blogapi/dto/request/AccountRequest.java
@@ -1,13 +1,17 @@
 package com.jeonghyeon.blogapi.dto.request;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 
 @Data
 public class AccountRequest {
+    @ApiModelProperty(example = "유저 아이디")
     private String userId;
+    @ApiModelProperty(example = "유저 비밀번호")
     private String userPassword;
+    @ApiModelProperty(example = "유저 이름")
     private String userName;
 
 }
--- src/main/java/com/jeonghyeon/blogapi/dto/request/LoginRequest.java
+++ src/main/java/com/jeonghyeon/blogapi/dto/request/LoginRequest.java
@@ -1,10 +1,13 @@
 package com.jeonghyeon.blogapi.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 
 @Data
 public class LoginRequest {
+    @ApiModelProperty(example = "유저 아이디")
     private String username;
+    @ApiModelProperty(example = "유저 비밀번호")
     private String password;
 }
